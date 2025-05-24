a1356392f18d355f32cece34038d39012c5f797a
Karol <karoljaimes187@gmail.com>
2025-05-23 19:27:56 -0500

    Actualización

---
+++ src/models/Categorias.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const categoriaSchema = mongoose.Schema(
+    {
+    Imagen : {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    CategoriaNombre: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    Descripcion: {
+        type: String,
+        required: true,
+        trim: true,
+    }
+},
+{
+    timestamps: true,
+}
+)
+
+const Categoria = mongoose.model("Categoria",categoriaSchema)
+
+export default Categoria;
